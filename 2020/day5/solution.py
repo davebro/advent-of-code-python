7a3ad5f953b5d83784acaf67bb488a2f81e3eff4
davebro <davebroers@gmail.com>
2020-12-05 11:03:22 -0800

    day five, part two

--- 2020/day5/solution.py
+++ 2020/day5/solution.py
@@ -7,27 +7,39 @@ def evaluate_letter(letter, positions, direction):
         high = high - ((high - low) / 2) - 1
     return low, high
 
-def part1():
+def part1(showOutput):
+    ids = []
     with open('input.txt') as file:
         boarding_passes = file.read().splitlines()
-        ids = []
         for boarding_pass in boarding_passes:
-            low, high = 0, 128
+            low, high = 0, 127
             row, column = 0, 0
             for index, letter in enumerate(boarding_pass[:7]):
                 if index == 6:
                     row = min(low, high) if letter == 'F' else max(low, high)
+                    low, high = 0, 7
                 else:
                     low, high = evaluate_letter(letter, [low, high], ['F', 'B'])
-            low, high = 0, 8
             for index, letter in enumerate(boarding_pass[-3:]):
                 if index == 2:
                     column = min(low, high) if letter == 'L' else max(low, high)
                 else:
                     low, high = evaluate_letter(letter, [low, high], ['L', 'R'])
             ids.append(row * 8 + column)
-        print(max(ids))
+        if showOutput:
+            print(max(ids))
+    return ids
 
-part1();
+def part2():
+    ids = part1(False)
+    ids.sort()
+    counter = ids[0]
+    for id in ids:
+        if id != counter:
+            break
+        counter += 1
+    print(counter)
 
+part1(True)
+part2()
 
